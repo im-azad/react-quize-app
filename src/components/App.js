d38ad14a6fd8fe86e4cec46de170cf739fd825e6
Abul Kalam Azad <me.azad99@gmail.com>
2022-07-14 23:33:37 +0600

    add signup page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import '../styles/App.css';
 import Home from './pages/Home';
 import Layout from './Layout';
+import Signup from './Signup';
 function App() {
 	return (
 		<Layout>
-			<Home />
+			{/* <Home /> */}
+			<Signup />
 		</Layout>
 	);
 }
